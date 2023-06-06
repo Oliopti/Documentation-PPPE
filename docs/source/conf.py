ef3aacf01a25297808405ebbd469c98d0684af86
Oliopti <olivier.jourdaintechnicien@gmail.com>
2023-06-06 17:33:10 +0200

    maj

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'PPPE'
+copyright = '2023, Olivier JOURDAIN'
+author = 'Olivier JOURDAIN'
 
 release = '0.1'
 version = '0.1.0'
